ef8c1313e844c2ea16abdfbde7683585aa51f5ed
dasananta <bec_ananta_it@yahoo.com>
2023-08-08 13:03:21 +0530

    merge issue fix

--- IBM_07AUG2023_D1MorBNew/Models/Product.cs
+++ IBM_07AUG2023_D1MorBNew/Models/Product.cs
@@ -25,69 +25,63 @@ namespace IBM_07AUG2023_D1MorBNew.Models
         static string JsonProductFileName = @"c:\temp\Products.json";
         static ProductLocalContext()
         {
-            static ProductLocalContext()
-            {
-                if (!File.Exists(JsonProductFileName))
-                {
-                    Products = new List<Product> { new Product { ProductID = 1000, ProductName = "BMW", Qty = 10 } };
-                    string jsonstring = Products.ToJson();
-                    File.WriteAllText(JsonProductFileName, jsonstring);
-                }
 
-                Products = JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText(JsonProductFileName));
+            if (!File.Exists(JsonProductFileName))
+            {
+                Products = new List<Product> { new Product { ProductID = 1000, ProductName = "BMW", Qty = 10 } };
+                string jsonstring = Products.ToJson();
+                File.WriteAllText(JsonProductFileName, jsonstring);
             }
 
-
-            public Product GetProductByID(int prdID)
-            {
-                return Products.SingleOrDefault(p => p.ProductID == prdID);
-            }
-
-            public IEnumerable<Product> GetAllProducts()
-            {
-                return Products;
-            }
-
-
-            public void AddNewProduct(Product product)
-            {
-                product.ProductID = Products.Max(p => p.ProductID) + 1;
-                Products.Add(product);
-                SaveData();
-            }
-
-
-            public void UpdateProduct(int prdid, Product product)
-            {
-                if (prdid != product.ProductID) return;
-
-                Product prd = Products.SingleOrDefault(p => p.ProductID == prdid);
-                int idx = Products.IndexOf(prd);
-                Products.Remove(prd);
-                Products.Insert(idx, product);
-                SaveData();
-            }
-
-            public void DeleteProduct(int prdid, Product product)
-            {
-                if (prdid != product.ProductID) return;
-
-                Product prd = Products.SingleOrDefault(p => p.ProductID == prdid);
-                int idx = Products.IndexOf(prd);
-                Products.Remove(prd);
-                Products.Insert(idx, product);
-                SaveData();
-            }
-
-
-            private void SaveData()
-            {
-                File.WriteAllText(JsonProductFileName, Products.ToJson());
-                //update the ctx
-                Products = JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText(JsonProductFileName));
-            }
-
+            Products = JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText(JsonProductFileName));
         }
 
 
+        public Product GetProductByID(int prdID)
+        {
+            return Products.SingleOrDefault(p => p.ProductID == prdID);
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return Products;
+        }
+
+        public void AddNewProduct(Product product)
+        {
+            product.ProductID = Products.Max(p => p.ProductID) + 1;
+            Products.Add(product);
+            SaveData();
+        }
+
+
+        public void UpdateProduct(int prdid, Product product)
+        {
+            if (prdid != product.ProductID) return;
+
+            Product prd = Products.SingleOrDefault(p => p.ProductID == prdid);
+            int idx = Products.IndexOf(prd);
+            Products.Remove(prd);
+            Products.Insert(idx, product);
+            SaveData();
+        }
+
+        public void DeleteProduct(int prdid, Product product)
+        {
+            if (prdid != product.ProductID) return;
+
+            Product prd = Products.SingleOrDefault(p => p.ProductID == prdid);
+            int idx = Products.IndexOf(prd);
+            Products.Remove(prd);
+            Products.Insert(idx, product);
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            File.WriteAllText(JsonProductFileName, Products.ToJson());
+            //update the ctx
+            Products = JsonConvert.DeserializeObject<List<Product>>(File.ReadAllText(JsonProductFileName));
+        }
     }
+}
